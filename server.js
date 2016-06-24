1779ea2721ed3295271fa144c76ec3021f695e2d
rajeshdhiman <rajeshdhimanskt@gmail.com>
2016-06-24 19:51:55 +0530

    added spiti,malana

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ server.listen(app.get('port'), function() {
  * attach a listener to new tweets with hashtags to watch 
  */
 function getNewTweets() {
-  var WATCH_HASHTAGS = '#himachal, #himachalpradesh, #Himachal, #HimachalPradesh, #हिमाचल, #shimla, #manali, #dharamshala, #dhauladhar, #himalayas, #devbhoomi, #birbilling, #kasol, #parvativalley';
+  var WATCH_HASHTAGS = '#himachal, #himachalpradesh, #Himachal, #HimachalPradesh, #हिमाचल, #shimla, #manali, #dharamshala, #spiti, #dhauladhar, #himalayas, #devbhoomi, #birbilling, #kasol, #parvativalley, #malana';
   /* 
    *  
    * filter the twitter public stream by the hashtags. 
